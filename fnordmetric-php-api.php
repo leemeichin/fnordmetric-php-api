16bdae45cac76935ea4b032089f91026727e8de2
leemachin <leemachin@ems-media.co.uk>
2012-01-23 19:23:33 +0000

    basic class skeleton

---
+++ fnordmetric-php-api.php
@@ -0,0 +1,30 @@
+<?php
+  class FnordmetricApi {
+    
+    protected $socket;
+    public    $host,
+              $port;
+
+    public function __construct($host = '0.0.0.0', $port = 1337) {
+
+    }
+
+    public function __destruct() {
+
+    }
+
+    public function event($name, Array $data = null) {
+    
+    }
+
+    public function openSocket() {
+
+    }
+
+    public function writeSocket() {
+
+    }
+
+    public function closeSocket() {
+
+    }
