c816024aec4a9c116d2b1a8d98458bbec9656acb
leemachin <leemachin@ems-media.co.uk>
2012-01-23 22:23:12 +0000

    suppressed errors on socket_* methods, as the exception catches them

--- fnordmetric-php-api.php
+++ fnordmetric-php-api.php
@@ -25,14 +25,14 @@
     }
 
     public function openSocket() {
-      $this->socket = socket_create(IF_ANET, SOCK_STREAM, SOL_TCP);
+      $this->socket = @socket_create(IF_ANET, SOCK_STREAM, SOL_TCP);
       if ($this->socket === false) throw new SocketConnectionErrorException;
 
       // add client connection to socket (ie. us)
-      $client = socket_bind($this->socket, '127.0.0.1');
+      $client = @socket_bind($this->socket, '127.0.0.1');
       if ($client === false) throw new SocketConnectionErrorException;
 
-      $server = socket_connect($this->socket, $this->host, $this->port);
+      $server = @socket_connect($this->socket, $this->host, $this->port);
       if ($server === false) throw new SocketConnectionErrorException;
     }
 
@@ -43,7 +43,7 @@
       $offset = 0;
 
       while ($offset < $length) {
-        $sent_data = socket_write($this->socket, substr($data, $offset), $length - $offset);
+        $sent_data = @socket_write($this->socket, substr($data, $offset), $length - $offset);
         if ($sent_data === false) throw new SocketConnectionErrorException;
 
         $offset += $sent_data;
